6b67556b4908f9c04c8286226cd60ec248b0bcfb
Sawraj <kdmswaraj17@gmail.com>
2024-10-11 22:56:48 +0530

    Added About Us page component

--- Astromanvi/src/App.jsx
+++ Astromanvi/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Landing from './Components/Landing'
+import AboutUs from './Components/AboutUs'
 
 function App() {
 
   return (
     <>
     <Landing/>
+    <AboutUs/>
         
     </>
   )
